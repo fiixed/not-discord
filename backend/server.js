8cdacc16774f648a5cc1aa1e2af9aa285356741e
Andrew Bell <fiixed@gmail.com>
2023-03-27 08:51:19 -0500

    server folder structure + auth routes

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const authRoutes = require('./routes/authRoutes');
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+// register the routes
+app.use('/api/auth', authRoutes);
+
 
 const server = http.createServer(app);
 
