1ff3435c8c66de879b35c55fd80650da2e5f6606
Andrew Bell <fiixed@gmail.com>
2023-03-30 13:38:17 -0500

    login, register, dashboard routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={<Navigate to="/dashboard" replace={true} />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
