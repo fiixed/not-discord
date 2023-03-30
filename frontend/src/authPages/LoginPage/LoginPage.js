db29143e8b74dc533e7fa27449481e7e1d3c3d0b
Andrew Bell <fiixed@gmail.com>
2023-03-30 14:06:08 -0500

    authbox component

--- frontend/src/authPages/LoginPage/LoginPage.js
+++ frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AuthBox from "../../shared/components/AuthBox";
 
 const LoginPage = () => {
-  return <div>
+  return <AuthBox>
     Login Page
-  </div>;
+  </AuthBox>;
 };
 
 export default LoginPage;
