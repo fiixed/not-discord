6768aa7513247ac6adcbee0a81c7241c98ff5f0c
Andrew Bell <fiixed@gmail.com>
2023-04-02 09:30:05 -0500

    Friends Sidebar

---
+++ frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { styled } from "@mui/system";
+//import PendingInvitationsListItem from "./PendingInvitationsListItem";
+
+const DUMMY_INVITATIONS = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Mark",
+      mail: "dummy@ad.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "John",
+      mail: "John@ad.com",
+    },
+  },
+];
+
+const MainContainer = styled("div")({
+  width: "100%",
+  height: "22%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  overflow: "auto",
+});
+
+const PendingInvitationsList = () => {
+  return (
+    <MainContainer>
+      
+        
+    </MainContainer>
+  );
+};
+
+export default PendingInvitationsList;
