4b29bdbb34786ec0ab8a98fc3ea53e4dc6d7582d
Andrew Bell <fiixed@gmail.com>
2023-04-02 11:28:27 -0500

    FriendsList

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { styled } from "@mui/system";
+import FriendsListItem from "./FriendsListItem";
 
+const DUMMY_FRIENDS = [
+  {
+    id: 1,
+    username: "Mark",
+    isOnline: true,
+  },
+  {
+    id: 2,
+    username: "Anna",
+    isOnline: false,
+  },
+  {
+    id: 3,
+    username: "John",
+    isOnline: false,
+  },
+];
 
 const MainContainer = styled("div")({
   flexGrow: 1,
@@ -10,7 +28,14 @@ const MainContainer = styled("div")({
 const FriendsList = () => {
   return (
     <MainContainer>
-      
+      {DUMMY_FRIENDS.map((f) => (
+        <FriendsListItem
+          username={f.username}
+          id={f.id}
+          key={f.id}
+          isOnline={f.isOnline}
+        />
+      ))}
     </MainContainer>
   );
 };
